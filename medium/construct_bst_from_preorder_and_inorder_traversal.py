e5a486875aaa171af4de1a961092446ce1eb65f2
Ray Liu <rayliu109@gmail.com>
2019-03-04 22:48:37 -0500

    added q105

---
+++ medium/construct_bst_from_preorder_and_inorder_traversal.py
@@ -0,0 +1,18 @@
+"""
+Given preorder and inorder traversal of a tree, construct the binary tree.
+Time: O(N^2)
+Space: O(logN)
+"""
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNodeEZ CLAP
+        """ 
+        if inorder:
+            index = inorder.index(preorder.pop(0))
+            root = TreeNode(inorder[index])
+            root.left = self.buildTree(preorder, inorder[0:index])
+            root.right = self.buildTree(preorder, inorder[index+1:])
+            return root
